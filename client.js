ad085f1731a2d13b7ce97b67d83f430cb9a9875e
leenaAhmed <leena3792@gmail.com>
2025-08-31 19:02:26 +0300

    add admin view

--- client.js
+++ client.js
@@ -1,19 +1,20 @@
 const baseURL = "http://localhost:7777/video-request";
 const videoListElm = document.getElementById("listOfRequests");
-
+const supperUserId = "68b46395b7852d09e8b035a7";
 const state = {
-  sortBy : "newFirst",
-  searchTerm : "",
-  filterBy: "all", 
-  userId: ''
-}
+  sortBy: "newFirst",
+  searchTerm: "",
+  filterBy: "all",
+  userId: "",
+  isSuperUser: false
+};
 
 function applyVoteStyle(videoId, videoList, voteType) {
   if (!voteType) {
     if (videoList.ups.includes(state.userId)) {
-      voteType ="ups"
-    }else if (videoList.downs.includes(state.userId)) {
-      voteType = "downs"
+      voteType = "ups";
+    } else if (videoList.downs.includes(state.userId)) {
+      voteType = "downs";
     } else {
       return;
     }
@@ -21,6 +22,14 @@ function applyVoteStyle(videoId, videoList, voteType) {
 
   const voteUpsElm = document.getElementById(`vote-ups-${videoId}`);
   const voteDownElm = document.getElementById(`vote-downs-${videoId}`);
+
+  if (state.isSuperUser) {
+    voteUpsElm.style.opacity = 0.5;
+    voteUpsElm.style.cursor = "not-allowed";
+    voteDownElm.style.opacity = 0.5;
+    voteDownElm.style.cursor = "not-allowed";
+    return
+  }
 
   const voteUpsDirElm = voteType === "ups" ? voteUpsElm : voteDownElm;
   const voteDownsDirElm = voteType === "ups" ? voteDownElm : voteUpsElm;
@@ -33,12 +42,43 @@ function applyVoteStyle(videoId, videoList, voteType) {
   }
 }
 
+function updateVideoStatus(id, status, resVideo) {
+  fetch(baseURL, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ id, status, resVideo })
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      window.location.reload()
+    });
+}
 
 function renderSingleVidReq(videoInfo, isPrepend = false) {
   const videoReqContainer = document.createElement("div");
-  const { _id, votes } = videoInfo;
+  const { _id, votes, status } = videoInfo;
   videoReqContainer.innerHTML = `
         <div class="card mb-3">
+        <div class="card-header d-flex justify-content-between">
+          <select id="admin_change_status_${_id}">
+            <option value="new">new</option>
+            <option value="planned">planned</option>
+            <option value="done">done</option>
+          </select>
+          <div class="input-group ml-2 mr-5 ${
+            status !== "done" ? "d-none" : ""
+          }" id="admin_video_res_container_${_id}">
+            <input type="text" class="form-control"
+              id="admin_video_res_${_id}" 
+              placeholder="paste here youtube video id">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" 
+                id="admin_save_video_res_${_id}"
+                type="button">Save</button>
+            </div>
+          </div>
+          <button id="admin_delete_video_req_${_id}" class='btn btn-danger'>delete</button>
+        </div>
           <div class="card-body d-flex justify-content-between flex-row">
             <div class="d-flex flex-column">
               <h3>${videoInfo.topic_title}</h3>
@@ -54,7 +94,9 @@ function renderSingleVidReq(videoInfo, isPrepend = false) {
             </div>
             <div class="d-flex flex-column text-center">
               <a class="btn btn-link" id="vote-ups-${_id}">🔺</a>
-              <h3  id="score-${_id}">${votes?.ups.length - votes?.downs.length}</h3>
+              <h3  id="score-${_id}">${
+    votes?.ups.length - votes?.downs.length
+  }</h3>
               <a class="btn btn-link"  id="vote-downs-${_id}">🔻</a>
             </div>
           </div>
@@ -82,58 +124,111 @@ function renderSingleVidReq(videoInfo, isPrepend = false) {
     videoListElm.append(videoReqContainer);
   }
 
+  const adminChangeStatusElm = document.getElementById(
+    `admin_change_status_${_id}`
+  );
+  const adminVideoResElm = document.getElementById(`admin_video_res_${_id}`);
+  const adminVideoResContainer = document.getElementById(
+    `admin_video_res_container_${_id}`
+  );
+  const adminSaveVideoResElm = document.getElementById(
+    `admin_save_video_res_${_id}`
+  );
+  const adminDeleteVideoReqElm = document.getElementById(
+    `admin_delete_video_req_${_id}`
+  );
+
+  adminChangeStatusElm.value = videoInfo.status;
+  adminVideoResElm.value = videoInfo.video_ref.link;
+
+  adminChangeStatusElm.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    if (e.target.value === "done") {
+      adminVideoResContainer.classList.remove("d-none");
+    } else {
+      updateVideoStatus(_id, e.target.value)
+    }
+  });
+
+  adminSaveVideoResElm.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (adminVideoResElm.value) {
+      updateVideoStatus(_id, 'done' ,adminVideoResElm.value)
+    } else {
+      adminVideoResElm.classList.add("is-invalid");
+      adminVideoResElm.addEventListener("input", () =>
+        adminVideoResElm.classList.remove("is-invalid")
+      );
+      return;
+    }
+  });
+
+  adminDeleteVideoReqElm.addEventListener('click', (e) => {
+    fetch(baseURL, {
+      method: "DELETE",
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify({ id: videoInfo._id }),
+    }).then(()=> window.location.reload())
+  })
+
   const voteScoreElm = document.getElementById(`score-${videoInfo._id}`);
 
   const votesElms = document.querySelectorAll(
-      `[id^="vote-"][id$="-${videoInfo._id}"]`
-    );
-  
-  votesElms.forEach(item => {
-    item.addEventListener('click', function (e) {
+    `[id^="vote-"][id$="-${videoInfo._id}"]`
+  );
+
+  votesElms.forEach((item) => {
+    if (state.isSuperUser) {
+      return
+    }
+    item.addEventListener("click", function (e) {
       e.preventDefault();
-      const [, vote_type , id ]= e.target.getAttribute('id').split('-');
+      const [, vote_type, id] = e.target.getAttribute("id").split("-");
       fetch(`${baseURL}/vote`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        id,
-        vote_type: vote_type,
-        user_id: state.userId
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id,
+          vote_type: vote_type,
+          user_id: state.userId
+        })
       })
-    })
-      .then((blob) => blob.json())
-      .then((res) => {
-        voteScoreElm.innerText = res.ups.length - res.downs.length;
-        applyVoteStyle(id , res, vote_type)
-      });
-    })
-  })
- 
+        .then((blob) => blob.json())
+        .then((res) => {
+          voteScoreElm.innerText = res.ups.length - res.downs.length;
+          applyVoteStyle(id, res, vote_type);
+        });
+    });
+  });
+
   return videoListElm;
 }
 
-
-
-function loadAllVidReqs(sortBy ="newFirst", searchTerm = "", filterBy = "all") {
-  fetch(`${baseURL}?sortBy=${sortBy}&searchTerm=${searchTerm}&filterBy=${filterBy}`)
+function loadAllVidReqs(
+  sortBy = "newFirst",
+  searchTerm = "",
+  filterBy = "all"
+) {
+  fetch(
+    `${baseURL}?sortBy=${sortBy}&searchTerm=${searchTerm}&filterBy=${filterBy}`
+  )
     .then((res) => res.json())
     .then((videoReq) => {
       videoListElm.innerHTML = "";
       videoReq.forEach((videoItem) => {
         renderSingleVidReq(videoItem);
-        applyVoteStyle(videoItem._id, videoItem.votes)
+        applyVoteStyle(videoItem._id, videoItem.votes);
       });
     });
 }
 
-
-function debounce(func , time){
+function debounce(func, time) {
   let timer;
 
-  return function(...args){
-    if(timer) clearTimeout(timer);
-    timer = setTimeout(()=> func.apply(this, args), time)
-  }
+  return function (...args) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), time);
+  };
 }
 
 function validateEmail(email) {
@@ -141,99 +236,109 @@ function validateEmail(email) {
   return re.test(email);
 }
 
-function validateForm(formData){
+function validateForm(formData) {
+  const topicTitle = formData.get("topic_title").trim();
+  const topicDetails = formData.get("topic_details").trim();
 
-   const topicTitle = formData.get("topic_title").trim();
-   const topicDetails = formData.get("topic_details").trim();
+  if (!topicTitle || topicTitle.length > 20) {
+    document.querySelector("[name=topic_title]").classList.add("is-invalid");
+  }
+  if (!topicDetails) {
+    document.querySelector("[name=topic_details]").classList.add("is-invalid");
+  }
 
-
-   if (!topicTitle || topicTitle.length > 20) {
-     document.querySelector('[name=topic_title]').classList.add('is-invalid')
-   }
-   if (!topicDetails) {
-     document.querySelector('[name=topic_details]').classList.add('is-invalid')
-   }
-
-   const formValidity = document.getElementById("sendVideoRequested").querySelectorAll('.is-invalid')
-   if (formValidity.length) {
-     formValidity.forEach(elm => {
-       elm.addEventListener('input', function() {
-         this.classList.remove('is-invalid');
-       });
-     });
-     return false;
-   }
-   return true;
+  const formValidity = document
+    .getElementById("sendVideoRequested")
+    .querySelectorAll(".is-invalid");
+  if (formValidity.length) {
+    formValidity.forEach((elm) => {
+      elm.addEventListener("input", function () {
+        this.classList.remove("is-invalid");
+      });
+    });
+    return false;
+  }
+  return true;
 }
 
-
 function loginFormVaildation(formData) {
-   const authorName = formData.get("author_name").trim();
-   const authorEmail = formData.get("author_email").trim();
+  const authorName = formData.get("author_name").trim();
+  const authorEmail = formData.get("author_email").trim();
 
-     if (!authorName) {
-     document.querySelector('[name=author_name]').classList.add('is-invalid')
-   }
-   if (!authorEmail || !validateEmail(authorEmail)) {
-     document.querySelector('[name=author_email]').classList.add('is-invalid')
-   }
+  if (!authorName) {
+    document.querySelector("[name=author_name]").classList.add("is-invalid");
+  }
+  if (!authorEmail || !validateEmail(authorEmail)) {
+    document.querySelector("[name=author_email]").classList.add("is-invalid");
+  }
 
-  const loginFormValidity = document.getElementById("loginUser").querySelectorAll('.is-invalid')
-   if (loginFormValidity.length) {
-     loginFormValidity.forEach(elm => {
-       elm.addEventListener('input', function() {
-         this.classList.remove('is-invalid');
-       });
-     });
-     return false;
-   }
-   return true;
+  const loginFormValidity = document
+    .getElementById("loginUser")
+    .querySelectorAll(".is-invalid");
+  if (loginFormValidity.length) {
+    loginFormValidity.forEach((elm) => {
+      elm.addEventListener("input", function () {
+        this.classList.remove("is-invalid");
+      });
+    });
+    return false;
+  }
+  return true;
+}
+
+function changeUserLayout() {
+  const videoRequestContentElm = document.querySelector(".app-content");
+  const userFormLoginElm = document.querySelector(".form-login");
+  const mainUserContentElm = document.querySelector(".normal-user-content");
+  state.userId = localStorage.getItem("current-user");
+  if (state.userId) {
+    loadAllVidReqs(state.sortBy, state.searchTerm, state.filterBy);
+    videoRequestContentElm.classList.remove("d-none");
+    userFormLoginElm.classList.add("d-none");
+    if (state.userId == supperUserId) {
+      state.isSuperUser = true;
+      mainUserContentElm.classList.add("d-none");
+    }
+  }
 }
 
 // once document loaded and before styling load
 document.addEventListener("DOMContentLoaded", () => {
   const videoRequestElm = document.getElementById("sendVideoRequested");
-  const sortElms = document.querySelectorAll('[id*=sort_by_]')
+  const sortElms = document.querySelectorAll("[id*=sort_by_]");
   const searchBy = document.getElementById("video_search");
 
-  const videoRequestContentELm = document.querySelector(".app-content")
-  const userFormLogin = document.querySelector(".form-login")
-  
   // const searchParams = new URLSearchParams(paramsString);
-   state.userId = localStorage.getItem("current-user")
-  if (state.userId) {
-    loadAllVidReqs(state.sortBy, state.searchTerm, state.filterBy);
-    videoRequestContentELm.classList.remove("d-none");
-    userFormLogin.classList.add("d-none");
-  }
 
+  changeUserLayout();
 
-  sortElms.forEach(items => {
-    items.addEventListener('click', function (e) {
+  sortElms.forEach((items) => {
+    items.addEventListener("click", function (e) {
       e.preventDefault();
-        state.sortBy = this.querySelector('input').value
-        loadAllVidReqs(state.sortBy, state.searchTerm)
-        this.classList.add('active')
-        if (state.sortBy == "topVotedFirst") {
-          document.getElementById('sort_by_new').classList.remove('active')
-        }else {
-          document.getElementById('sort_by_top').classList.remove('active')
-        }
-    })
-  })
+      state.sortBy = this.querySelector("input").value;
+      loadAllVidReqs(state.sortBy, state.searchTerm);
+      this.classList.add("active");
+      if (state.sortBy == "topVotedFirst") {
+        document.getElementById("sort_by_new").classList.remove("active");
+      } else {
+        document.getElementById("sort_by_top").classList.remove("active");
+      }
+    });
+  });
 
- 
-  searchBy.addEventListener("input", debounce((e) =>{
-    state.searchTerm = e.target.value;
-    loadAllVidReqs(state.sortBy, state.searchTerm);
-  }, 1500) );
-
+  searchBy.addEventListener(
+    "input",
+    debounce((e) => {
+      state.searchTerm = e.target.value;
+      loadAllVidReqs(state.sortBy, state.searchTerm);
+    }, 1500)
+  );
 
   videoRequestElm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(videoRequestElm);
-    formData.append("author_id", state.userId)
-   
+    formData.append("author_id", state.userId);
+
     // check validation
     const isValid = validateForm(formData);
     if (!isValid) return;
@@ -244,25 +349,25 @@ document.addEventListener("DOMContentLoaded", () => {
     })
       .then((blob) => blob.json())
       .then((data) => {
-        renderSingleVidReq(data , true);
+        renderSingleVidReq(data, true);
       });
   });
-
 
   const loginUserElm = document.getElementById("loginUser");
 
   loginUserElm.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(loginUserElm);
-    
+
     fetch("http://localhost:7777/users/login", {
       method: "POST",
       body: formData
     })
-      .then((blob) => blob.json()).then((data) => {
+      .then((blob) => blob.json())
+      .then((data) => {
         state.userId = data._id;
-        localStorage.setItem("current-user", data._id)
+        localStorage.setItem("current-user", data._id);
+        changeUserLayout();
       });
   });
-
 });
